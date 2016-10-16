5a9c8857d4031d5c5830a81038e9ea953834b51a
Philipp Gorczak <p.gorczak@gmail.com>
2016-10-16 23:32:02 +0200

    select 2

--- select.py
+++ select.py
@@ -1,3 +1,5 @@
+import codecs
+
 import appex
 import dialogs
 
@@ -5,7 +7,7 @@ import utils
 
 fp = appex.get_file_path()
 if fp:
-    with open(fp, 'r') as f:
+    with codecs.open(fp, 'r', 'utf-8') as f:
         org_str = f.read()
 
     elements = tuple(utils.iter_agenda(org_str))
@@ -14,4 +16,4 @@ if fp:
 
     selected = tuple(items[s] for s in dialogs.list_dialog(titles))
 
-    print selected
+    print(selected)
