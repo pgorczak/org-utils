c8101847d8e338dce4caa56e25f0878a3278cac3
Philipp Gorczak <p.gorczak@gmail.com>
2016-10-16 23:52:28 +0200

    select 3

--- select.py
+++ select.py
@@ -7,13 +7,13 @@ import utils
 
 fp = appex.get_file_path()
 if fp:
-    with codecs.open(fp, 'r', 'utf-8') as f:
+    with codecs.open(fp, 'r', 'utf-16') as f:
         org_str = f.read()
 
     elements = tuple(utils.iter_agenda(org_str))
     titles = tuple(e['headline'] for e in elements)
     items = dict(zip(titles, elements))
 
-    selected = tuple(items[s] for s in dialogs.list_dialog(titles))
+    selected = tuple(items[s] for s in dialogs.list_dialog(items=titles))
 
     print(selected)
